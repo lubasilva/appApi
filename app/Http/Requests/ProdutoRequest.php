fed4dfd30aa5eccb8627f2ffaf48465cc1411ff3
Lucas <lubapisilva@gmail.com>
2022-03-03 09:46:57 -0300

    cria requests

---
+++ app/Http/Requests/ProdutoRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProdutoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'id' => 'required',
+            // 'sequencia' => 'required',
+            'nome' => 'required',
+            'qtdvendida' => 'required',
+            'qtdentregue' => 'required',
+            // 'campo1' => 'required',
+            // 'campo2' => 'required',
+            // 'campo3' => 'required'
+        ];
+    }
+}
