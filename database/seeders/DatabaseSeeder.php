36ab90b5aac489d13869414e62420935d548fcdc
Lucas <lubapisilva@gmail.com>
2022-03-03 09:44:53 -0300

    adiciona seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\PedidoDeVenda;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,5 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call([
+            ClienteSeeder::class,
+            PedidoSeeder::class,
+            UnidadeSeeder::class,
+            ProdutoSeeder::class,
+            PedidoDeVendaSeeder::class,
+        ]);
     }
 }
